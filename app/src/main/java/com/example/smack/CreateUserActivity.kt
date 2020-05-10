f3f5ae5e73259558848eecf9e3124022e5656031
C047437 <manoharan.vellaichamy@usbank.com>
2020-05-10 22:45:32 +0530

    lesson 4

---
+++ app/src/main/java/com/example/smack/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package com.example.smack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun createUserBtnClicked(view: View) {
+
+    }
+
+    fun generateColorBtnClicked(view: View) {
+
+    }
+
+    fun createAvatarImageClicked(view: View) {
+
+    }
+}
